0ed0ba8c23a33daa59909f81c049ce160d418040
sudeepthiketineni <sudeepthiketineni@gmail.com>
2020-09-16 12:42:22 +0530

    Making language selection dropdown

--- RDIP/Codes/sentence.js
+++ RDIP/Codes/sentence.js
@@ -1,4 +1,15 @@
-
+var array1;
+var array2;
+var strUser
+function array() {
+var e = document.getElementById("ar");
+strUser = e.options[e.selectedIndex].text;
+document.getElementById("r").innerHTML="Form a sentence (Declarative or Interrogative or any other type) from the given words";
+document.getElementById("s").innerHTML="(select the buttons in proper order)";
+if(strUser=="---select a language---"){
+  alert("select language");
+}
+if(strUser=="English"){
   array1=[["John ate an apple before afternoon","before afternoon John ate an apple","John before afternoon ate an apple"],
 ["some students like to study in the night","at night some students like to study"],
 ["John and Mary went to church","Mary and John went to church"],
@@ -9,7 +20,7 @@
 ["John ate an apple so did she","she ate an apple so did John"],
 ["the teacher returned the book after she noticed the error","the teacher noticed the error after she returned the book","after the teacher returned the book she noticed the error","after the teacher noticed the error she returned the book","she returned the book after the teacher noticed the error","she noticed the error after the teacher returned the book","after she returned the book the teacher noticed the error","after she noticed the error the teacher returned the book"],
 ["I told her that I bought a book yesterday","I told her yesterday that I bought a book","yesterday I told her that I bought a book","I bought a book that I told her yesterday","I bought a book yesterday that I told her","yesterday I bought a book that I told her"]];
-
+}
 else if(strUser=="Hindi"){
   array2=[["राम और श्याम बाजार गयें","राम और श्याम गयें बाजार","बाजार गयें राम और श्याम","गयें बाजार राम और श्याम"],
 ["राम सोया और श्याम भी","श्याम सोया और राम भी","सोया श्याम और राम भी","सोया राम और श्याम भी"],
@@ -18,3 +29,11 @@ else if(strUser=="Hindi"){
 ["बिल्लियों को मारकर कुत्ता सो गया","मारकर बिल्लियों को कुत्ता सो गया","बिल्लियों को मारकर सो गया कुत्ता","मारकर बिल्लियों को सो गया कुत्ता","कुत्ता सो गया बिल्लियों को मारकर","कुत्ता सो गया मारकर बिल्लियों को","सो गया कुत्ता बिल्लियों को मारकर","सो गया कुत्ता मारकर बिल्लियों को"],
 ["एक लाल किताब वहाँ है","एक लाल किताब है वहाँ","वहाँ है एक लाल किताब","है वहाँ एक लाल किताब"],
 ["एक बड़ी सी किताब वहाँ है","एक बड़ी सी किताब है वहाँ","बड़ी सी एक किताब वहाँ है","बड़ी सी एक किताब है वहाँ","वहाँ है एक बड़ी सी किताब","वहाँ है बड़ी सी एक किताब","है वहाँ एक बड़ी सी किताब","है वहाँ बड़ी सी एक किताब"]];
+}
+}
+function get_answers(){
+  ans="";
+  document.getElementById("answers").innerHTML="";
+  var n=0;
+  
+}
